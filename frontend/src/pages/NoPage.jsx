7d57f41d156e2f061f3b2bc125f8859b28bff84e
Shubham121s <sp6063512@gmail.com>
2024-09-13 13:48:19 +0530

    first commit

---
+++ frontend/src/pages/NoPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen  text-gray-800 text-2xl">
+      <p className='text-white'>Page Not Found</p>
+    </div>
+  );
+};
+
+export default NoPage;
+
